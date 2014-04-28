e1d60bd330a3869a2d6eb28aa030b060f3221974
gulakov <Sasha101>
2014-04-28 16:35:04 -0500

    class

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 # Python Glider
-# Glider simualting Conway's Game of Life or A New Kind of Science "computational universe" thesis 
-
+# Glider simulating Conway's Game of Life or A New Kind of Science "computational universe" thesis 
 import os
 import time
 
-#dimensions
-d=30 
-w=[[0 for j in range(d)] for i in range(d)]
+class GOL:
+	def __init__(self):
+		self.d=30
+		self.w = [[0 for j in range(30)] for i in range(30)]
+		#setup glider
+		self.w[0][1]=1
+		self.w[1][2]=1
+		self.w[2][0]=1
+		self.w[2][1]=1
+		self.w[2][2]=1
 
-#setup glider
-w[0][1]=1
-w[1][2]=1
-w[2][0]=1
-w[2][1]=1
-w[2][2]=1
-
-
-if __name__ == "__main__":
-
-	#g = int(input("Gen:"))
-	g=100
-
-	for e in range(g):
+	#display board
+	def show(self):
 		os.system('cls')
 
-		#display board
-		for r in range(len(w)):
+		for r in range(len(self.w)):
 			disp=""
-			for c in range(len(w[r])):
-				disp+="#" if w[r][c]==1 else " "
+			for c in range(len(self.w[r])):
+				disp+="#" if self.w[r][c]==1 else " "
 			print(disp)
 			
 		time.sleep(0.1) 
 
+	def next(self):
+		w=self.w
+		d=self.d
 		w2 =[[0 for j in range(d)] for i in range(d)]
-
 
 		for r in range(len(w)):
 			for c in range(len(w[r])):
@@ -64,4 +59,10 @@ if __name__ == "__main__":
 					w2[r][c]=w[r][c]
 				if(n==3):
 					w2[r][c]=1
-		w=w2
+		self.w=w2
+
+if __name__ == "__main__":
+	g = GOL()
+	for e in range(100):
+		g.show()
+		g.next()
